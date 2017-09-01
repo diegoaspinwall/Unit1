6988bd9a98eeb9d723c6cbc456343eb6565181ed
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-01 14:58:14 -0400

    Updated from Brython Server: 9/1/2017 2:58:15 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,10 @@
+#Diego Aspinwall
+#9-1-17
+#stringAnalysis.py
 
+sentence = input('Enter a sentence: ')
+print('Your sentence has', sentence.count(' ')+1, 'words and', len(sentence), 'characters and', len(sentence)-sentence.count(' '), 'letters')
+charsearch = input('Enter a character to search for: ')
+print('Your sentence has', sentence.count(charsearch), 'of the character', charsearch)
+wordsearch = input('Enter a word to search for: ')
+print('Your sentence has', sentence.count(wordsearch), 'of the word', wordsearch)
