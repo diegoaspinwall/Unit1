11694107f9b77d481821b01fa5ed393056d66581
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-06 13:47:45 -0400

    Updated from Brython Server: 9/6/2017 1:47:44 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,8 @@
+#Diego Aspinwall
+#9-6-17
+#eggs.py
 
+eggnum = int(input('Input number of eggs: '))
+cartnum = int(input('Input number of cartons: '))
+cartonreal = eggnum // cartnum +1
+print(cartonreal == cartnum)
