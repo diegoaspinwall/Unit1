10c557f474299418a4a428520ecfeef9712c27e0
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-06 13:58:56 -0400

    Updated from Brython Server: 9/6/2017 1:58:55 PM

--- change.py
+++ change.py
@@ -1,1 +1,10 @@
+#Diego Aspinwall
+#9-6-17
+#change.py
 
+centtotal = int(input('Input number of cents: '))
+quarters = centtotal//25
+dimes = (centtotal - centtotal//25*25)//10
+nickles = centtotal - (((centtotal - centtotal//25*25)//10*10) + (centtotal - centtotal//25*25)//5)
+
+print(quarters, dimes, nickles)
