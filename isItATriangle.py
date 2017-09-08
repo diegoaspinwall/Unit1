c0879e2e7d6b8757f2c07e9d2c9e1dc50bf0bb7b
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-08 14:41:31 -0400

    Updated from Brython Server: 9/8/2017 2:41:31 PM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,11 @@
+#Diego Aspinwall
+#9-8-17
+#isItATriangle.py
 
+a = float(input('Enter Side A: '))
+b = float(input('Enter Side B: '))
+c = float(input('Enter Side C: '))
+maximum = max(a,b,c)
+minimum = min(a,b,c)
+middle = (a+b+c)-(maximum+minimum)
+print((minimum+middle)> maximum)
