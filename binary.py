5924b50308fee1a7fbd59fec194c19f92dee6434
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-08 15:13:41 -0400

    Updated from Brython Server: 9/8/2017 3:13:41 PM

--- binary.py
+++ binary.py
@@ -2,6 +2,9 @@
 #9-8-17
 #binary.py
 
+base10test = int(input('Input base 10 number: '))
+print(bin(base10test))
+
 digfull = int(input('Enter an 8-digit binary number: '))
 dig8 = (digfull%10)*2**0
 dig7 = ((digfull//10)%10)*2**1
@@ -12,6 +15,3 @@ dig3 = ((digfull//100000)%10)*2**5
 dig2 = ((digfull//1000000)%10)*2**6
 dig1 = ((digfull//10000000)%10)*2**7
 print(dig8+dig7+dig6+dig5+dig4+dig3+dig2+dig1)
-
-base10test = int(input('Input base 10 number: '))
-print(bin(base10test))
