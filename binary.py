d7eaf85b80437133f2ba80f00dfebfc4fc9a0872
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-08 15:05:46 -0400

    Updated from Brython Server: 9/8/2017 3:05:46 PM

--- binary.py
+++ binary.py
@@ -3,5 +3,12 @@
 #binary.py
 
 digfull = int(input('Enter an 8-digit binary number: '))
-dig8 = (digfull%.1)*2**0
-
+dig8 = (digfull%10)*2**0
+dig7 = ((digfull//10)%10)*2**1
+dig6 = ((digfull//100)%10)*2**2
+dig5 = ((digfull//1000)%10)*2**3
+dig4 = ((digfull//10000)%10)*2**4
+dig3 = ((digfull//100000)%10)*2**5
+dig2 = ((digfull//1000000)%10)*2**6
+dig1 = ((digfull//10000000)%10)*2**7
+print(dig8+dig7+dig6+dig5+dig4+dig3+dig2+dig1)
