41ef62ab6f1d6827897927bde62724decdf7a53e
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-11 14:21:40 -0400

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,10 @@
+#Diego Aspinwall
+#9-11-17
+#quiz1.py - our first quiz
 
+print('Diego Aspinwall')
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+print('The sum is', num1+num2)
+print('The product is', num1*num2)
+print('Your lucky number is', (7*num1)+(7*num2))
